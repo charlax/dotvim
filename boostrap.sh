c7eb6c1b9960ba81534635ffe3c2a82018da6d6d
Charles-Axel Dein <ca@d3in.org>
2011-12-01 18:33:50 -0800

    added installation

--- boostrap.sh
+++ boostrap.sh
@@ -1,4 +1,11 @@
 # Add .old to any existing Vim file in the home directory
+
+function die()
+{
+    echo "${@}"
+    exit 1
+}
+
 for i in $HOME/.vimrc.before $HOME/.vimrc.after $HOME/.gvimrc.before $HOME/.gvimrc.after; do
   if [[ ( -e $i ) || ( -h $i ) ]]; then
     echo "${i} has been renamed to ${i}.old"
@@ -6,6 +13,11 @@ for i in $HOME/.vimrc.before $HOME/.vimrc.after $HOME/.gvimrc.before $HOME/.gvim
   fi
 done
 
-ln -s $HOME/.vimrc.perso/vimrc.before $HOME/.vimrc.before
-ln -s $HOME/.vimrc.perso/vimrc.after $HOME/.vimrc.after
-ln -s $HOME/.vimrc.perso/gvimrc.after $HOME/.gvimrc.after
+ln -s $HOME/.vimrc-perso/vimrc.before $HOME/.vimrc.before
+ln -s $HOME/.vimrc-perso/vimrc.after $HOME/.vimrc.after
+ln -s $HOME/.vimrc-perso/gvimrc.after $HOME/.gvimrc.after
+
+
+# Clone dotvim into .vimrc-perso
+git clone -b experimental git@github.com:charlax/dotvim.git $HOME/.vimrc-perso \
+  || die "Could not clone the repository to ${HOME}/.vim"
